9c638239876c0d2db1cfa5dcc45cae56ff0e379a
McKade <mckades@gmail.com>
2020-03-02 20:02:05 -0900

    Fixed typo

--- get_stats.py
+++ get_stats.py
@@ -8,21 +8,22 @@ def main(file: str) -> None:
 
     words_list = [line.rstrip('\n') for line in open(file)]
     status = Status()
-    for _ in words_list:
+    for word in words_list:
         start = time.time()
+        print(f"{word}\n")
         end = time.time()
 
         if .00001 <= end - start < .0000999999:
-            print(f"{start - end}")
+            print(f"Time {end - start}")
             status.l2_cache += 1
         elif end - start < .0000099999:
-            print(f"{start - end}")
+            print(f"Time {end - start}")
             status.l1_cache += 1
         elif .00599999 > end - start >= .0001:
-            print(f"{start - end}")
+            print(f"Time {start - start}")
             status.ram += 1
         elif end - start >= .006:
-            print(f"{start - end}")
+            print(f"Time {end - start}")
             status.tbl_miss += 1
 
     status.print_stats()

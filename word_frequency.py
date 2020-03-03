3b792c52d354389494e20ab4d77f95cfc7102148
McKade <mckades@gmail.com>
2020-03-02 19:31:11 -0900

    cleaned up code

--- word_frequency.py
+++ word_frequency.py
@@ -112,13 +112,10 @@ def display_frequency_data(arguments: Namespace) -> None:
     start_time = time.time()
     string_of_words = open_file(arguments.file)
     words = break_up_words(string_of_words)
-    # words_one, words_two, words_three = break_up_list(words)
 
-    words_stats_one = run_get_word_frequency_with_threads(words)
-    # words_stats_two = run_get_word_frequency_with_threads(words_two)
-    # words_stats_three = run_get_word_frequency_with_threads(words_two)
+    words_stats = run_get_word_frequency_with_threads(words)
 
-    words_stats = words_stats_one
+    words_stats = words_stats
     high_low_values = find_high_low_values(words_stats)
     random_words = get_random_words(words_stats)
     print_frequency_level(random_words, high_low_values)
